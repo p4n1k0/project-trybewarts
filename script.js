e9c16802238798ef4fd5b551bb47b98ab2ccae1b
DevOjr <deoliveirajr1@gmail.com>
2022-02-25 16:31:20 -0300

    Requisito 3

---
+++ script.js
@@ -0,0 +1,12 @@
+const email = document.querySelector('#email');
+const senha = document.querySelector('#senha');
+const button = document.querySelector('#button');
+
+function validaLogin() {
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+    window.alert('Olá, Tryber!');
+  }
+  window.alert('Email ou senha inválidos.');
+}
+
+button.addEventListener('click', validaLogin);
